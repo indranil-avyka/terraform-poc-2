b773f50807b6793a3e82cfd205f78e7da5f4fdbf
Indranil Saha <indranil.r.saha@avyka.com>
2026-06-26 05:27:17 +0530

    fix: Add subscription_id to backend and remove use_cli from provider

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "indraniltfstate123"
     container_name       = "tfstate"
     key                  = "randomtexts.tfstate"
+    subscription_id      = "25010899-5732-45e8-822d-b3e179ad1d0c"
   }
 }
--- provider.tf
+++ provider.tf
@@ -13,6 +13,5 @@ provider "azurerm" {
   subscription_id                 = var.subscription_id
   environment                     = "public"
   use_msi                         = false
-  use_cli                         = true
   use_oidc                        = false
 }
