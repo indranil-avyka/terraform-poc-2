9088b2b93164677a61c78534e597abb4291005ce
Indranil Saha <indranil.r.saha@avyka.com>
2026-06-26 05:31:58 +0530

    fix: Edit storage name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-avyka-users-temp"
-    storage_account_name = "indraniltfstate123"
+    storage_account_name = "indraniltfstate"
     container_name       = "tfstate"
     key                  = "randomtexts.tfstate"
     subscription_id      = "25010899-5732-45e8-822d-b3e179ad1d0c"
