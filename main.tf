9f9794c52a96a8992df6a2fa0c99a48f25f01bf6
Indranil Saha <223164790+indranil-avyka@users.noreply.github.com>
2026-06-26 12:51:11 +0530

    Add tags to the main.tf resource

--- main.tf
+++ main.tf
@@ -6,4 +6,7 @@ resource "azurerm_storage_account" "res-0" {
   min_tls_version                 = "TLS1_0"
   name                            = var.storage_account_name
   resource_group_name             = var.resource_group_name
+  tags = {
+    ManagedBy = "Harness"
+  }
 }
